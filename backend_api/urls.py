6ab8c2e96f3df2a733f0a26d44afd34deec961fc
Jordan-Burgess <alfredjordan93@gmail.com>
2023-05-03 13:41:56 -0400

    Create URL for AllSongs

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.conf import settings 
 from django.conf.urls.static import static 
 # For the API
-urlpatterns = [path("users/", views.Users.as_view(), name="users" ),
-               path("users/<int:id>/", views.ProfileView.as_view(), name="userShowPage")
-               ] 
+urlpatterns = [
+    path("users/", views.Users.as_view(), name="users" ),
+    path("users/<int:id>/", views.ProfileView.as_view(), name="userShowPage"),
+    path("songs/", views.AllSongs.as_view(), name="songs"),
+] 
