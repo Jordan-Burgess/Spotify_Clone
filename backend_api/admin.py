52da71ec7a816d76dab2daeb67d814937a45a273
Jordan-Burgess <alfredjordan93@gmail.com>
2023-05-03 13:44:46 -0400

    Register Song on Admin Site

--- backend_api/admin.py
+++ backend_api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Song
 
 # The User model is already imported from Django
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Song)
