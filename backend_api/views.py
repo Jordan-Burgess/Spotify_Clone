c6d45733024fd77f671c95a2ab3a028c10d231ba
Jordan-Burgess <alfredjordan93@gmail.com>
2023-05-03 13:39:01 -0400

    Add serializer to AllSongs view

--- backend_api/views.py
+++ backend_api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import User, Profile, Song
-from .serializers import UserSerializer, ProfileSerializer
+from .serializers import UserSerializer, ProfileSerializer, SongSerializer
 from django.http import JsonResponse
 # Create your views here.
 # get the data as a database as a json and use it in the frontend 
@@ -22,3 +22,5 @@ class ProfileView(APIView):
 class AllSongs(APIView):
     def get(self, request):
         data = Song.objects.all()
+        serializer = SongSerializer(data, many=True)
+        return JsonResponse(serializer.data, safe=False)
